21296d780ea4f307cce97e1b31c479eead2dc799
Dossja <domka_24@interia.pl>
2021-07-11 13:19:59 +0200

    Python test routes

--- Python-MySQL/app/main.py
+++ Python-MySQL/app/main.py
@@ -5,4 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "<h1>Hello World!</h1>"
+
+
+@app.route("/cli")
+def index_cli():
+    return "<h1>CLI!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0',)
