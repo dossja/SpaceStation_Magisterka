3046d809a6edaee9f7818459940f6570cf61fd86
Dossja <domka_24@interia.pl>
2021-07-23 21:30:08 +0200

    CORS added and axios installed

--- Python-MySQL/app/files/__init__.py
+++ Python-MySQL/app/files/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@172.17.0.1/space_station'
 db = SQLAlchemy(app)
 db.create_all()
@@ -10,4 +12,3 @@ db.create_all()
 # login_manager.login_view = 'login'
 # login_manager.login_message_category = 'info'
 from files.routes import user_routes, report_routes
-db.create_all()
--- Python-MySQL/frontend/src/App.js
+++ Python-MySQL/frontend/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
