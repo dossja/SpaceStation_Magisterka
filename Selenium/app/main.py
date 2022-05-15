48bb955f8c7375d774ce55b5d9d3684fbfe36ae6
Dossja <domka_24@interia.pl>
2022-05-15 11:15:42 +0200

    First login test

--- Selenium/app/main.py
+++ Selenium/app/main.py
@@ -1,8 +1,12 @@
 from time import sleep
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from login_test import test_succesful_login
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
+PAGE = "http://localhost:3000"
 driver = webdriver.Chrome(PATH)
 
 # sleep(5)
@@ -11,6 +15,6 @@ driver = webdriver.Chrome(PATH)
 
 # driver = webdriver.Remote('http://selenium:4444', options=options)
 
-driver.get('https://python.org')
-sleep(5)
+driver.get(PAGE)
+test_succesful_login(driver)
 driver.quit()
---
+++ Selenium/app/reports_test.py
@@ -0,0 +1,8 @@
+from time import sleep
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_reports_btn(driver):
+    sleep(10)
