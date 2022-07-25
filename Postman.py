97f8ac466589edd72b5cbbfc4dd8acc0aa68c2fe
Dossja <domka_24@interia.pl>
2022-07-25 09:56:33 +0200

    Updated script

--- Postman.py
+++ Postman.py
@@ -6,43 +6,52 @@ import sys
 
 data = {}
 language = sys.argv[1]
-resources = sys.argv[2]
 
-with open(f"SpaceStation_{language}_{resources}.postman_test_run.json", "r") as read_file:
-    data = json.load(read_file)
+with open(f"SpaceStation_{language}_Max.postman_test_run.json", "r") as read_file:
+    data_max = json.load(read_file)
 
-results = data["results"]
+results_max = data_max["results"]
 
+with open(f"SpaceStation_{language}_Med.postman_test_run.json", "r") as read_file:
+    data_med = json.load(read_file)
 
-path = f"{language}_{resources}_Postman.xlsx"
+results_med = data_med["results"]
+
+with open(f"SpaceStation_{language}_Min.postman_test_run.json", "r") as read_file:
+    data_min = json.load(read_file)
+
+results_min = data_min["results"]
+
+path = f"{language}_Postman.xlsx"
 
 book = openpyxl.Workbook()
 if os.path.isfile(path):
     book = openpyxl.load_workbook(path)
 
-iter = 0
+sum_max = 0.0
+sum_med = 0.0
+sum_min = 0.0
 
-for el in results:
+for i in range(len(results_max)):
     sheetName = ""
 
-    if (iter == 0 or iter == 1 or iter == 2 or iter == 3):
+    if (i == 0 or i == 1 or i == 2 or i == 3):
         sheetName = "Reports"
-    if (iter == 4 or iter == 5 or iter == 6):
+    if (i == 4 or i == 5 or i == 6):
         sheetName = "Incidents"
-    if (iter == 7 or iter == 8 or iter == 9):
+    if (i == 7 or i == 8 or i == 9):
         sheetName = "Missions"
-    if (iter == 10 or iter == 11 or iter == 12 or iter == 13):
+    if (i == 10 or i == 11 or i == 12 or i == 13):
         sheetName = "MissionCrew"
-    if (iter == 14):
+    if (i == 14):
         sheetName = "PositionTypes"
-    if (iter == 15):
+    if (i == 15):
         sheetName = "ReportStatuses"
-    if (iter == 16):
+    if (i == 16):
         sheetName = "ReportTypes"
-    if (iter == 17 or iter == 18 or iter == 19 or iter == 20 or iter == 21 or iter == 22):
+    if (i == 17 or i == 18 or i == 19 or i == 20 or i == 21 or i == 22):
         sheetName = "Users"
 
-    iter += 1
     isSheet = False
 
     for sh in book:
@@ -54,15 +63,30 @@ for el in results:
 
     sheet = book[sheetName]
 
-    time = float(el['times'][0]) * 0.001
+    time_max = float(results_max[i]['times'][0]) * 0.001
+    sum_max += time_max
+    time_med = float(results_med[i]['times'][0]) * 0.001
+    sum_med += time_med
+    time_min = float(results_min[i]['times'][0]) * 0.001
+    sum_min += time_min
 
-    sheet.append([el['name']])
-    sheet.append(["", "Time"])
-    sheet.append(["", time])
+    sheet.append([results_max[i]['name']])
+    sheet.append(["", "", "Time"])
+    sheet.append(["", "MAX", time_max])
+    sheet.append(["", "MED", time_med])
+    sheet.append(["", "MIN", time_min])
     sheet.append([""])
 
 if book.sheetnames[0] == "Sheet":
     sheet_to_del = book['Sheet']
     book.remove(sheet_to_del)
 
-book.save(f"{language}_{resources}_Postman.xlsx")
+book.create_sheet("Summary")
+sheet = book["Summary"]
+
+sheet.append(["", "Sum"])
+sheet.append(["MAX", sum_max])
+sheet.append(["MED", sum_med])
+sheet.append(["MIN", sum_min])
+
+book.save(f"{language}_Postman.xlsx")
